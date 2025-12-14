8419db430ed30d5168e4eca51287ff6841067897
Patrick MacDonald <patrick@patmac.ca>
2025-12-14 16:33:50 -0700

    fixed build errors after update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { AuthKitProvider } from '@workos-inc/authkit-nextjs';
+import { AuthKitProvider } from '@workos-inc/authkit-nextjs/components';
 
 
 const geistSans = Geist({
